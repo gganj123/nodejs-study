b27b4dcc4b140591ef93ed419118d130d7744641
gganj123 <zwol10@naver.com>
2024-10-15 13:54:23 +0900

    study : mongoDB-firstDB homework 11

--- mongodb-test/app.js
+++ mongodb-test/app.js
@@ -5,105 +5,34 @@ const client = new MongoClient(uri);
 
 async function run() {
   const database = client.db("firstDB");
-  const inventory = database.collection("inventory");
+  const students = database.collection("students");
 
-  //   const userData = await users.insertOne({ name: "dony", age: 17 });
-  //   console.log("result", userData);
+  //문제 11. updateOne
+  //students 컬렉션에 해당 데이터를 넣자
 
-  //   const userList = [
-  //     { name: "철수", age: 30 },
-  //     { name: "jessica", age: 25 },
-  //   ];
-  //   const userListResult = await users.insertMany(userList);
-  //   console.log("result", userListResult);
-
-  //   const findUser = await users.findOne({ age: { $gt: 20 } });
-  //   console.log("result", findUser);
-
-  //   const UpdateUser = await users.updateOne(
-  //     { name: "dony" },
-  //     { $set: { age: 18 } }
-  //   );
-  //   console.log("result", UpdateUser);
-
-  //   const DeleteUser = await users.deleteOne({ name: "dony" });
-  //   console.log("result", DeleteUser);
-
-  //   const userData = await users
-  //     .find({ age: { $gt: 20 } })
-  //     .project({ name: 1 })
-  //     .toArray();
-  //   console.log("userdata", userData);
-
-  //문제4 inventory에 해당데이터를 먼저 넣고
-  //   const goods = await inventory.insertMany([
+  //   const insertStudent = await students.insertMany([
   //     {
-  //       item: "journal",
-  //       qty: 25,
-  //       size: { h: 14, w: 21, uom: "cm" },
-  //       status: "A",
+  //       _id: 1,
+  //       test1: 95,
+  //       test2: 92,
+  //       test3: 90,
+  //       modified: new Date("01/05/2020"),
   //     },
   //     {
-  //       item: "notebook",
-  //       qty: 50,
-  //       size: { h: 8.5, w: 11, uom: "in" },
-  //       status: "A",
+  //       _id: 2,
+  //       test1: 98,
+  //       test2: 100,
+  //       test3: 102,
+  //       modified: new Date("01/05/2020"),
   //     },
-  //     {
-  //       item: "paper",
-  //       qty: 100,
-  //       size: { h: 8.5, w: 11, uom: "in" },
-  //       status: "D",
-  //     },
-  //     {
-  //       item: "planner",
-  //       qty: 75,
-  //       size: { h: 22.85, w: 30, uom: "cm" },
-  //       status: "D",
-  //     },
-  //     {
-  //       item: "postcard",
-  //       qty: 45,
-  //       size: { h: 10, w: 15.25, uom: "cm" },
-  //       status: "A",
-  //     },
+  //     { _id: 3, test1: 95, test2: 110, modified: new Date("01/04/2020") },
   //   ]);
-  //   console.log("result", goods);
-
-  //문제4-2 status가 D인 데이터를 찾아라
-  //   const findGoods = await inventory.find({ status: "D" }).toArray();
-  //   console.log(findGoods);
-
-  //문제5 status가 'A’이고 qty가 50인 데이터를 찾아라
-  //   const findGoods = await inventory.find({ status: "A", qty: 50 }).toArray();
-  //   console.log(findGoods);
-
-  //문제 6. 쿼리 $in 사용해보기
-  //   const findGoods = await inventory
-  //     .find({ status: { $in: ["A", "B"] } })
-  //     .toArray();
-  //   console.log(findGoods);
-
-  //문제 7. $lt
-  //   const findGoods = await inventory
-  //     .find({ status: "A", qty: { $lt: 30 } })
-  //     .toArray();
-  //   console.log(findGoods);
-
-  //문제 8. $or
-  //   const findGoods = await inventory
-  //     .find({ $or: [{ status: "A", qty: { $lt: 30 } }] })
-  //     .toArray();
-  //   console.log(findGoods);
-
-  // 문제 9. nested field
-  // size 에 uom이 in 인 데이터를 찾자
-  //   const findGoods = await inventory.find({ "size.uom": "in" }).toArray();
-  //   console.log(findGoods);
-
-  //문제 10.size에 h가 10을 초과하는 데이터를 찾자
-  const findGoods = await inventory.find({ "size.h": { $gt: 10 } }).toArray();
-  console.log(findGoods);
+  //문제 11-1. id가 3인 학생에게 test3 의 점수를 98로 세팅하자
+  const updateStudent = await students.updateOne(
+    { _id: 3 },
+    { $set: { test3: 98 } }
+  );
+  console.log(updateStudent);
 }
 
 run();
