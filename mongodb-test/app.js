171d4225f07622fa76e12d1c8a6def51c11d0d82
gganj123 <zwol10@naver.com>
2024-10-15 12:26:36 +0900

    study : mongoDB-firstDB homework 5

--- mongodb-test/app.js
+++ mongodb-test/app.js
@@ -6,7 +6,36 @@ const client = new MongoClient(uri);
 async function run() {
   const database = client.db("firstDB");
   const inventory = database.collection("inventory");
-  //문제4
+
+  //   const userData = await users.insertOne({ name: "dony", age: 17 });
+  //   console.log("result", userData);
+
+  //   const userList = [
+  //     { name: "철수", age: 30 },
+  //     { name: "jessica", age: 25 },
+  //   ];
+  //   const userListResult = await users.insertMany(userList);
+  //   console.log("result", userListResult);
+
+  //   const findUser = await users.findOne({ age: { $gt: 20 } });
+  //   console.log("result", findUser);
+
+  //   const UpdateUser = await users.updateOne(
+  //     { name: "dony" },
+  //     { $set: { age: 18 } }
+  //   );
+  //   console.log("result", UpdateUser);
+
+  //   const DeleteUser = await users.deleteOne({ name: "dony" });
+  //   console.log("result", DeleteUser);
+
+  //   const userData = await users
+  //     .find({ age: { $gt: 20 } })
+  //     .project({ name: 1 })
+  //     .toArray();
+  //   console.log("userdata", userData);
+
+  //문제4 inventory에 해당데이터를 먼저 넣고
   //   const goods = await inventory.insertMany([
   //     {
   //       item: "journal",
@@ -40,39 +69,14 @@ async function run() {
   //     },
   //   ]);
   //   console.log("result", goods);
-  //문제5
+
+  //문제4-2 status가 D인 데이터를 찾아라
   //   const findGoods = await inventory.find({ status: "D" }).toArray();
   //   console.log(findGoods);
-
   
-
-  //   const userData = await users.insertOne({ name: "dony", age: 17 });
-  //   console.log("result", userData);
-
-  //   const userList = [
-  //     { name: "철수", age: 30 },
-  //     { name: "jessica", age: 25 },
-  //   ];
-  //   const userListResult = await users.insertMany(userList);
-  //   console.log("result", userListResult);
-
-  //   const findUser = await users.findOne({ age: { $gt: 20 } });
-  //   console.log("result", findUser);
-
-  //   const UpdateUser = await users.updateOne(
-  //     { name: "dony" },
-  //     { $set: { age: 18 } }
-  //   );
-  //   console.log("result", UpdateUser);
-
-  //   const DeleteUser = await users.deleteOne({ name: "dony" });
-  //   console.log("result", DeleteUser);
-
-  //   const userData = await users
-  //     .find({ age: { $gt: 20 } })
-  //     .project({ name: 1 })
-  //     .toArray();
-  //   console.log("userdata", userData);
+  //문제5 status가 'A’이고 qty가 50인 데이터를 찾아라
+  const findGoods = await inventory.find({ status: "A", qty: 50 }).toArray();
+  console.log(findGoods);
 }
 
 run();
