bc35ee72a53b7ebd1e72b17085542d222d7851cf
gganj123 <zwol10@naver.com>
2024-10-16 18:11:50 +0900

    study : email 유효성 체크

--- new-mongoose/app.js
+++ new-mongoose/app.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/mongoose-test");
-
+const validator = require("validator");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -9,9 +9,16 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        if (!value.includes("@")) throw new Error("This is not an Email");
+      },
+    },
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
@@ -22,9 +29,8 @@ const User = mongoose.model("User", userSchema);
 
 const newUser = new User({
   name: "dony",
-  email: "gganj123@gmail.com",
+  email: "zzznj123@gmail.com",
   password: "12345",
-  age: 25,
 });
 
 newUser.save().then((value) => console.log("value is", value));
